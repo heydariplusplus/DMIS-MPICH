8ee19783a0a7f55130ce83fe52a91e4c011b28d8
Hasan Heydari <39858314+heydariplusplus@users.noreply.github.com>
2018-06-03 10:42:51 +0430

    add comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,25 @@
 
 int main(int argc, char* argv[])
 {
-	MPI_Init(&argc, &argv);
+	MPI_Init(&argc, &argv);	
 
-	int rank;
+	int rank;	//the unique identifier is assigned to the process.
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if(argc==3)
+	if(argc == 3)	//check if the entered command is valid or not
 	{
-		string inputDirectory = argv[1];
+		string inputDirectory = argv[1];	
 		string inputPath = inputDirectory + "/" + to_string(rank) + ".txt";
 		string logDirectory = argv[2];
 
-		Node* node = new Node(rank, new MisAlgorithm(logDirectory), inputPath);
+		Node* node = new Node(rank, new MisAlgorithm(logDirectory), inputPath);	//instantiate an object from class Node	
 	}
 	else
 	{
 		cout << "Command line arguments are incorrect";
 	}
 
-	MPI_Finalize();
+	MPI_Finalize();	 
 
 	return 0;
 }
