d9f4c6f8cdde6799dcbf89cdcb5cd2a51df0f8f4
Hasan Heydari <39858314+heydariplusplus@users.noreply.github.com>
2018-06-03 12:02:54 +0430

    Update misAlgorithm.cpp

--- misAlgorithm.cpp
+++ misAlgorithm.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <chrono>
 
+//This function finds the maximum of an array
 double Max(double* array, int lenght)
 {
 	double max = 0;
@@ -12,12 +13,14 @@ double Max(double* array, int lenght)
 	return max;
 }
 
+//This function finds the index of an item in an array
 int GetIndex(vector<int>* vec, int value)
 {
 	for (int i = 0; i < vec->size(); ++i)if (vec->at(i) == value)return i;
 	return -1;
 }
 
+//This function generates a random double number
 double RandomRange(int min, int max)
 {
 	mt19937 rng;
@@ -42,11 +45,11 @@ void MisAlgorithm::Run(int id, vector<int>* vecNeiId)
 	srand(time(NULL) + id);
 
 	int iteration = 1;
-	while (NodeStatus == Status::Unknown)
+	while (NodeStatus == Status::Unknown)	//the main loop of the algorithm. As long as the status of the node is Unknown (is not Mis or ComMis), this loop continues.
 	{
 		Log(id, "", iteration++);
 
-		if (vecNeiId->size() == 0)
+		if (vecNeiId->size() == 0)	//if a node does not have any neighbor, it is added to the MIS set.
 		{
 			NodeStatus = Status::Mis;
 			return;
@@ -63,6 +66,7 @@ void MisAlgorithm::Run(int id, vector<int>* vecNeiId)
 	Log(id, "---Finish---");
 }
 
+//This method shows the result (status of the node after termination of the algorithm).
 void MisAlgorithm::ShowResult(int id)
 {
 	string status;
@@ -79,7 +83,7 @@ void MisAlgorithm::ShowResult(int id)
 	}
 
 	string msg = "Node " + to_string(id) + " is " + status;
-	cout << msg;
+	cout << msg << endl;
 	Log(id, msg);
 }
 
@@ -88,6 +92,7 @@ Status MisAlgorithm::GetNodeStatus()
 	return NodeStatus;
 }
 
+//This method writes the logs on a file
 void MisAlgorithm::Log(int id, string message, int iteration)
 {
 	ofstream outFile;
@@ -99,11 +104,12 @@ void MisAlgorithm::Log(int id, string message, int iteration)
 	outFile.close();
 }
 
+//In this method, the node generates a random double number in [0, 1] and sends it to all of its neighbors  
 double MisAlgorithm::Round1(int id, vector<int>* vecNeiId)
 {
 	Log(id, "...Round 1 Start...");
 
-	double randNum = RandomRange(0, 1);
+	double randNum = RandomRange(0, 1);		//generate a random number
 
 	Log(id, " randNum: " + to_string(randNum));
 
@@ -115,18 +121,19 @@ double MisAlgorithm::Round1(int id, vector<int>* vecNeiId)
 	return randNum;
 }
 
+//In this method, the node receives the generated random numbers of neighbors and find the maximum of them.
 double MisAlgorithm::Round2(int id, vector<int>* vecNeiId)
 {
 	Log(id, "...Round 2 Start...");
 
-	double* arrRecRandNum = new double[vecNeiId->size()];
+	double* arrRecRandNum = new double[vecNeiId->size()];	//an array that will contain the random numbers of the neighbors
 	for (int i = 0; i < vecNeiId->size(); i++)
 	{
 		double recRandNum;
 		MPI_Recv(&recRandNum, 1, MPI_DOUBLE, vecNeiId->at(i), MessageTag::RandNum, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		arrRecRandNum[i] = recRandNum;
 	}
-	double maxRecRandNum = Max(arrRecRandNum, vecNeiId->size());
+	double maxRecRandNum = Max(arrRecRandNum, vecNeiId->size());	//find the maximum random number among reveived random numbers.
 	delete[] arrRecRandNum;
 
 	Log(id, " maxRecRandNum: " + to_string(maxRecRandNum));
@@ -136,6 +143,7 @@ double MisAlgorithm::Round2(int id, vector<int>* vecNeiId)
 	return maxRecRandNum;
 }
 
+//In this method, if the random number of the node is greater than all of the neighbors' random numbers, the status of the node changes to Mis 
 void MisAlgorithm::Round3(int id, vector<int>* vecNeiId, double& randNum, double& maxRecRandNum)
 {
 	Log(id, "...Round 3 Start...");
