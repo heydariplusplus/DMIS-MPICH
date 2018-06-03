1e0c706b8fad1169c68101bdc04428cae9f5b7d5
Hasan Heydari <39858314+heydariplusplus@users.noreply.github.com>
2018-06-03 10:59:18 +0430

    Update algorithm.h

--- algorithm.h
+++ algorithm.h
@@ -1,3 +1,11 @@
+/*
+This class is an abstract class called IAlgorithm. 
+Since some versions of c++ does not supprt interface (__interface), abstract class is used.
+It is used for implementing a dependency injection. 
+Objects of Node class can run some algorithms (note that in this program, each object of Node class runs only one algorithm called 
+MisAlgorithm, but the Node class is written in a way that it could be further developed.), so dependency injection technique is usued.
+*/
+
 #pragma once
 #include "stdafx.h"
 
